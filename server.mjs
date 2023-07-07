85eb3bb2bf8cc78d46e4451e84197b6a4426ce8f
DragonMaiden06 <ampb0624@gmail.com>
2023-07-07 19:30:27 +0800

    Install dotenv

--- server.mjs
+++ server.mjs
@@ -1,2 +1,6 @@
 import express from 'express';
+import config from './configs/app-default.mjs'
 let app = express();
+
+app.use(express.json());
+app.listen(config.port, () => console.log(`Listening to port ${config.port}`));
